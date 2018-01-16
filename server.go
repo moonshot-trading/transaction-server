b43ec93f186412b6209ea752630951d0932ec5db
Tanner Zinck <tzinck@uvic.ca>
2018-01-16 13:51:26 -0800

    minimal quote endpoint working

--- server.go
+++ server.go
@@ -3,14 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net"
+	"bufio"
+	"time"
+	"strconv"
+	"math/rand"
 	"encoding/json"
 	"database/sql"
 
 	_ "github.com/lib/pq"
 )
 
+type Quote struct {
+	Price int
+	StockSymbol string
+	UserId string
+	Timestamp int
+	CryptoKey int
+}
+
 var (
-	quoteServerURL = "http://localhost:44417"
+	quoteServerURL = "localhost"
+	quoteServerPort = 44415
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,25 +35,48 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 func quoteHandler(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	req := struct {
-		userId string
-		stockSymbol string
+		UserId string
+		StockSymbol string
 	}{"", ""}
 
 	err := decoder.Decode(&req)
 
-	if err != nil {
+	if err != nil || len(req.StockSymbol) < 3 {
 		failWithStatusCode(err, http.StatusText(http.StatusBadRequest), w, http.StatusBadRequest)
+		return
 	}
 
-	//	Check for well formed stock symbol
+	commandString := req.StockSymbol + "," + req.UserId
+	quote := Quote{}
 
 	//	Check if the user exists, and if they have the necessary balance to request a quote
 
 	//	Make request to the quote service
+	conn, err := net.Dial("tcp", "localhost:44415")
+	failOnError(err, "Unable to connect to quote service")
+
+	fmt.Fprintf(conn, commandString + "\n")
+	quotedPrice, _ := bufio.NewReader(conn).ReadString('\n')
+
+	//	Apply the charge to the users account in the database
 
 	//	Return quote back to requester
-	
-	//	Apply the charge to the users account in the database
+	priceString, err := strconv.ParseInt(quotedPrice, 10, 64)
+	quote.Price = int(priceString)
+	quote.StockSymbol = req.StockSymbol
+	quote.UserId = req.UserId
+	//	Need to update the test quote server to generate this timestamp and cryptokey
+	quote.Timestamp = int(time.Now().Unix())
+	quote.CryptoKey = rand.Intn(99999999 - 10000000) + 10000000
+
+	quoteJson, err := json.Marshal(quote)
+	if err != nil {
+		failWithStatusCode(err, http.StatusText(http.StatusBadRequest), w, http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, string(quoteJson))
 }
 
 func loadDB() *sql.DB {
@@ -58,6 +95,7 @@ func loadDB() *sql.DB {
 }
 
 func main() {
+	rand.Seed(time.Now().Unix())
 	_ = loadDB()
 	port := ":44416"
 	fmt.Printf("Listening on port %s\n", port)
