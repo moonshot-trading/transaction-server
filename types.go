17b9d55b9974679e697aa6147fd1e1d56ec63752
Tanner Zinck <tzinck@uvic.ca>
2018-01-23 11:09:52 -0800

    add types for auditing

--- types.go
+++ types.go
@@ -18,3 +18,58 @@ type Sell struct {
 	SellAmount		int
 	StockSellAmount	int
 }
+
+//	Auditing types
+type AccountTransaction struct {
+	Server		string		`json:"server"`
+	Action		string		`json:"action"`
+	Username	string		`json:"username"`
+	Funds		int			`json:"funds"`
+}
+
+type SystemEvent struct {
+	Server		string		`json:"server"`
+	Command		string		`json:"command"`
+	StockSymbol	string		`json:"stockSymbol"`
+	Username	string		`json:"username"`
+	Filename	string		`json:"filename"`	
+	Funds		int			`json:"funds"`
+}
+
+type ErrorEvent struct {
+	Server			string		`json:"server"`
+	Command			string		`json:"command"`
+	StockSymbol		string		`json:"stockSymbol"`
+	Filename		string		`json:"filename"`	
+	Funds			int			`json:"funds"`
+	Username		string		`json:"username"`
+	ErrorMessage	string		`json:"errorMessage"`
+}
+
+type DebugEvent struct {
+	Server			string		`json:"server"`
+	Command			string		`json:"command"`
+	StockSymbol		string		`json:"stockSymbol"`
+	Filename		string		`json:"filename"`	
+	Funds			int			`json:"funds"`
+	Username		string		`json:"username"`
+	DebugMessage	string		`json:"debugMessage"`	
+}
+
+type QuoteServer struct {
+	Server			string		`json:"server"`
+	Price			int			`json:"price"`
+	StockSymbol		string		`json:"stockSymbol"`
+	Username		string		`json:"username"`
+	QuoteServerTime	int			`json:"quoteServerTime"`	
+	Cryptokey		string		`json:"cryptokey"`
+}
+
+type UserCommand struct {
+	Server			string		`json:"server"`
+	Command			string		`json:"command"`
+	Username		string		`json:"username"`
+	StockSymbol		string		`json:"stockSymbol"`
+	Filename		string		`json:"filename"`	
+	Funds			int			`json:"funds"`	
+}
